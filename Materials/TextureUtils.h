fc7ca2e035d2dd3cb7d0acd9f78474c54731294e
Lnorri <lnorri.fullsail@gmail.com>
2024-08-30 15:45:51 -0400

    Update TextureUtils.h
    Switched to Gateware Macro. Safer for windows.h

--- Materials/TextureUtils.h
+++ Materials/TextureUtils.h
@@ -49,7 +49,7 @@ void UploadTextureToGPU(GW::GRAPHICS::GVulkanSurface _surface, const tinygltf::I
 	GvkHelper::copy_buffer(vkDev, vkCmdPool, vkQGX, staging_bufferIM, _outTextureBuffer, imageSize);
 
 	VkExtent3D tempExtent = { _img.width, _img.height, 1 };
-	uint32_t mipLevels = static_cast<uint32_t>( floor( log2( std::max(_img.width, _img.height))) + 1);
+	uint32_t mipLevels = static_cast<uint32_t>( floor( log2( G_LARGER(_img.width, _img.height))) + 1);
 	GvkHelper::create_image(vkPDev, vkDev, tempExtent, mipLevels, VK_SAMPLE_COUNT_1_BIT, format, VK_IMAGE_TILING_OPTIMAL, 
 		VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
 		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, nullptr, &_outTextureImage, &_outTextureMemory);
