dc2a78869bf7f0604b8eaae0be5e5ac7a46905e3
unknown <natalie9705@gmail.com>
2024-10-16 19:17:52 -0400

    auto commit

--- FragmentShader.hlsl
+++ FragmentShader.hlsl
@@ -15,20 +15,24 @@ cbuffer other_data
     vector lightDir, camPos;
 };
 
+Texture2D texture0 : register(t0, space2);
+SamplerState sampler0 : register(s0, space2);
+
 float4 main(OUTPUT2 input) : SV_TARGET
 {
     //temp hard coded data - till the texture data is used in lab 6
-    static float4 diffuse = { 1.0f, 1.0f, 1.0f, 0.0f };
+    //static float4 diffuse = { 1.0f, 1.0f, 1.0f, 0.0f };
     static float4 specular = { 1.0f, 1.0f, 1.0f, 1.0f };
     static float4 emissive = { 0.0f, 0.0f, 0.0f, 1.0f };
     static float4 ambient = { 0.1f, 0.1f, 0.1f, 1.0f };
     static float ns = 140.0f;
     
+    float4 textureColour = texture0.Sample(sampler0, input.texCoord.xy);
     
     float3 norm = normalize(input.normW);
     float3 lightDirection = normalize(lightDir);
     float diffuseIntensity = saturate(dot(norm, -lightDirection));
-    float4 finalColour = (diffuseIntensity + ambient) * diffuse * lightColour;
+    float4 finalColour = (diffuseIntensity + ambient) * textureColour * lightColour;
     
     //half-vector method
     float3 viewDir = normalize(camPos.xyz - input.posW);
