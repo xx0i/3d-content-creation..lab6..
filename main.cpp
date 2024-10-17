3ec45fe0abd5410242926a0f5406ba5708acc1de
unknown <natalie9705@gmail.com>
2024-10-17 11:10:43 -0400

    auto commit

--- main.cpp
+++ main.cpp
@@ -43,11 +43,11 @@ int main()
 			"VK_LAYER_KHRONOS_validation", // standard validation layer
 			//"VK_LAYER_RENDERDOC_Capture" // add this if you have installed RenderDoc
 		};
-		if (+vulkan.Create(	win, GW::GRAPHICS::DEPTH_BUFFER_SUPPORT, 
+		if (+vulkan.Create(	win, GW::GRAPHICS::DEPTH_BUFFER_SUPPORT | GW::GRAPHICS::BINDLESS_SUPPORT, 
 							sizeof(debugLayers)/sizeof(debugLayers[0]),
 							debugLayers, 0, nullptr, 0, nullptr, false))
 #else
-		if (+vulkan.Create(win, GW::GRAPHICS::DEPTH_BUFFER_SUPPORT))
+		if (+vulkan.Create(win, GW::GRAPHICS::DEPTH_BUFFER_SUPPORT | GW::GRAPHICS::BINDLESS_SUPPORT))
 #endif
 		{
 			Renderer renderer(win, vulkan);
