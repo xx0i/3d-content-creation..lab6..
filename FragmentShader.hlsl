22e190647f1698287c7883543985dc690fa5012a
unknown <natalie9705@gmail.com>
2024-10-17 14:42:16 -0400

    auto commit

--- FragmentShader.hlsl
+++ FragmentShader.hlsl
@@ -27,7 +27,7 @@ float4 main(OUTPUT2 input) : SV_TARGET
     static float4 ambient = { 0.1f, 0.1f, 0.1f, 1.0f };
     static float ns = 140.0f;
     
-    float4 textureColour = textures[2].Sample(samplers[0], input.texCoord.xy);
+    float4 textureColour = textures[0].Sample(samplers[0], input.texCoord.xy);
     float textureRoughness = textures[1].Sample(samplers[0], input.texCoord.xy).r;
     
     float3 norm = normalize(input.normW);
