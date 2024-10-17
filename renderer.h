4e80b62803a45ac30b6263f7de9a879fd0b69a41
unknown <natalie9705@gmail.com>
2024-10-17 12:12:42 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -535,7 +535,6 @@ private:
 			imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
 			imageInfo.imageView = textures[i].imageView;
 			imageInfo.sampler = textureSampler;
-			infos.push_back(imageInfo);
 
 			VkWriteDescriptorSet writeTextureDescriptor = {};
 			writeTextureDescriptor.descriptorCount = 1;
@@ -548,7 +547,7 @@ private:
 			writeTextureDescriptor.pNext = nullptr;
 			writeTextureDescriptor.pTexelBufferView = nullptr;
 			writeTextureDescriptor.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-			writeTextureDescriptor.pImageInfo = infos.data();
+			writeTextureDescriptor.pImageInfo = &imageInfo;
 			vkUpdateDescriptorSets(device, 1, &writeTextureDescriptor, 0, nullptr);
 		}
 	}
